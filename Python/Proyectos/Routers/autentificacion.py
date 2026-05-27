604d8a243995aa65be162e9b49645b0f3ef949fe
Kevin <kevins030723@gmail.com>
2026-05-27 11:22:26 -0400

    cambios 2

--- Python/Proyectos/Routers/autentificacion.py
+++ Python/Proyectos/Routers/autentificacion.py
@@ -52,4 +52,17 @@ async def login(form: OAuth2PasswordRequestForm = Depends(), db=Depends(conexion
     access_token = crear_access_token(data = {"sub": user["usuario"]})
     return {"access_token": access_token, "token_type": "bearer"}
 
- 
+@router.post("/login_admin")
+async def login_admin(form: OAuth2PasswordRequestForm = Depends(), db=Depends(conexion)):
+    user = buscar_usuario_BD(form.username, db)
+    if not user:
+        raise HTTPException(status_code=400, detail="Usuario no encontrado")
+    
+    if not verificar_password(form.password, user["contrasena"]):
+        raise HTTPException(status_code=400, detail="Contraseña incorrecta")
+    
+    if user["rol"] != "admin":
+        raise HTTPException(status_code=403, detail="Acceso restringido solo a administradores")
+    
+    access_token = crear_access_token(data={"sub": user["usuario"], "rol": user["rol"]})
+    return {"access_token": access_token, "token_type": "bearer"}
